f530c4213a06316098e05c9f17efe8a5c91ad029
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2026-05-18 21:59:35 +0300

    Added Random class usage

---
+++ src/L04_GuessTheNumber/src/Program.java
@@ -0,0 +1,14 @@
+void main() {
+    Scanner input = new Scanner(System.in);
+    Random random = new Random();
+    int number = random.nextInt(1, 6);
+
+    IO.println("Aklımdan bir sayı tuttum. 1 ile 6 arasında. Tek hakkın var.");
+    int guess = input.nextInt();
+
+    if (guess == number) {
+        IO.println("Bravo! Tek seferde bildin.");
+    } else {
+        IO.println("Üzgünüm :(\nAklımdaki sayı = " + number);
+    }
+}
