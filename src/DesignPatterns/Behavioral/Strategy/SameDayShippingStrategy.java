c984117ff33871d9d9993a6508477754a544f4ec
buraksenyurt <burakselimsenyurt@gmail.com>
2026-08-07 17:39:30 +0300

    Implement shipping strategies using Strategy design pattern

---
+++ src/DesignPatterns/Behavioral/Strategy/SameDayShippingStrategy.java
@@ -0,0 +1,9 @@
+package DesignPatterns.Behavioral.Strategy;
+
+public class SameDayShippingStrategy implements ShippingStrategy {
+    @Override
+    public double calculateShipping(Order order) {
+        return 50 + order.getWeightKg() * 2.0;
+    }
+    
+}
